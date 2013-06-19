d09bae1486eace2df630658828354a6116712f4d
Paulo Vítor S Reis <paulovitin@gmail.com>
2013-06-19 12:22:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ require 'bootstrap.php';
 $bar = $em->find('Bar', 1);
 $foo = $bar->getFoo();
 
-var_dump($foo->getId());
+var_dump($foo->getId()); //retorna 1
 echo "\n";
 
 $foo_serial = serialize($foo);
 $foo = unserialize($foo_serial);
 
 echo "\n";
-var_dump($foo->getId());
+var_dump($foo->getId()); //retorna 0
